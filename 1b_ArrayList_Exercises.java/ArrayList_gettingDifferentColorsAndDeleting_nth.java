fd4ddd1f0c8279c84166bd47051185772d7af556
saniyaVaturi <vaturisaniya31@gmail.com>
2025-05-21 16:22:30 +0530

    Add files via upload

---
+++ 1b_ArrayList_Exercises.java/ArrayList_gettingDifferentColorsAndDeleting_nth.java
@@ -0,0 +1,39 @@
+package java_Assignment;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ArrayList_gettingDifferentColorsAndDeleting_nth {
+
+
+	    public static void main(String[] args) {
+	        // Create an ArrayList to store colors
+	        List<String> colors = new ArrayList<>();
+
+	        // Add some colors to the ArrayList
+	        colors.add("Red");
+	        colors.add("Green");
+	        colors.add("Blue");
+	        colors.add("Yellow");
+	        colors.add("Orange");
+
+	        System.out.println("Initial list of colors: " + colors);
+
+	        // Get the index of the element to be deleted from the user
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter the index of the element to delete (starting from 0): ");
+	        int n = scanner.nextInt();
+
+	        // Check if the index is valid
+	        if (n >= 0 && n < colors.size()) {
+	            // Remove the element at the specified index
+	            String removedColor = colors.remove(n);
+	            System.out.println("Removed color at index " + n + ": " + removedColor);
+	            System.out.println("List of colors after deletion: " + colors);
+	        } else {
+	            System.out.println("Invalid index. Please enter an index within the bounds of the list.");
+	        }
+
+	        scanner.close();
+	    }
+	}
