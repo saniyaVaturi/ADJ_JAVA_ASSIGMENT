09a07323f134234f896204432eb2239952b1570f
saniyaVaturi <vaturisaniya31@gmail.com>
2025-05-21 16:04:51 +0530

    Add files via upload

---
+++ 1b_ArrayList_Exercises.java/gettingDifferentColors_And_checking_red_is_available.java
@@ -0,0 +1,25 @@
+package java_Assignment;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class gettingDifferentColors_And_checking_red_is_available {
+
+
+
+    public static void main(String[] args) {
+        // Initialize an ArrayList to store colors
+        List<String> colors = new ArrayList<>();
+//adding elements
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Yellow");
+      //getting different colors
+        System.out.println("getting different colors:"+ colors);
+        //checking if red is available or not
+        System.out.println("is red available:"+colors.contains("Red"));
+    }
+
+   
+    }
+
