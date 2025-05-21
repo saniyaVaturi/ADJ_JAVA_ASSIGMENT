c719e9a7b112e1018fab710a9f07ffa4c3e83be6
saniyaVaturi <vaturisaniya31@gmail.com>
2025-05-21 22:02:23 +0530

    Add files via upload

---
+++ 5d_Swing.java/fiveD_CMYTabbedPane.java
@@ -0,0 +1,31 @@
+package Fifth;
+import javax.swing.*;
+import java.awt.*;
+public class fiveD_CMYTabbedPane extends JFrame{
+    public fiveD_CMYTabbedPane() {
+        JTabbedPane tabbedPane = new JTabbedPane();
+
+        JPanel cyanPanel = new JPanel();
+        cyanPanel.setBackground(Color.CYAN);
+
+        JPanel magentaPanel = new JPanel();
+        magentaPanel.setBackground(Color.MAGENTA);
+
+        JPanel yellowPanel = new JPanel();
+        yellowPanel.setBackground(Color.YELLOW);
+
+        tabbedPane.add("Cyan", cyanPanel);
+        tabbedPane.add("Magenta", magentaPanel);
+        tabbedPane.add("Yellow", yellowPanel);
+
+        add(tabbedPane);
+        setTitle("CMY Tabs");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new fiveD_CMYTabbedPane();
+    }
+}
