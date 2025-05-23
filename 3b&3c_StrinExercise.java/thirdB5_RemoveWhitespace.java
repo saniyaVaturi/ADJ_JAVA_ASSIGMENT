44fadedf0ce469c9f5303b0a7a2f0f76f33d9291
saniyaVaturi <vaturisaniya31@gmail.com>
2025-05-23 16:44:11 +0530

    thirdB5_RemoveWhitespace.java

--- 3b&3c_StrinExercise.java/thirdB5_RemoveWhitespace.java
+++ 3b&3c_StrinExercise.java/thirdB5_RemoveWhitespace.java
@@ -1,3 +1,5 @@
+/*Q5. Write a Java Program for Eliminating all whitespace characters from a string using user defined
+function removeWhitespace()*/
 package thirdB;
 
 public class thirdB5_RemoveWhitespace {
