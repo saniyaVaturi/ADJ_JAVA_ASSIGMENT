a12b65b9b8e0f5667c6abe3677c8134299bcef10
saniyaVaturi <vaturisaniya31@gmail.com>
2025-05-23 16:40:16 +0530

    SecondB_StringBuffer_And_StringBuilder.java

--- 2b_StringBuffer_StringBuilder.java/SecondB_StringBuffer_And_StringBuilder.java
+++ 2b_StringBuffer_StringBuilder.java/SecondB_StringBuffer_And_StringBuilder.java
@@ -1,3 +1,7 @@
+/*2.b. Develop a Java program to make a Performance Test on StringBuffer and StringBuilder for
+appending the string ‘AIET’ for 10000 times both in the StringBuffer and StringBuilder. Justify
+your answer which one is better.*/
+
 package secondQuestion;
 
 public class SecondB_StringBuffer_And_StringBuilder {
