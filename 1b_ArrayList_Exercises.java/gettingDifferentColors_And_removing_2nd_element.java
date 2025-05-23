44de7e440639b34367711ece5fc881563a540930
saniyaVaturi <vaturisaniya31@gmail.com>
2025-05-23 16:33:25 +0530

    gettingDifferentColors_And_removing_2nd_element.java

--- 1b_ArrayList_Exercises.java/gettingDifferentColors_And_removing_2nd_element.java
+++ 1b_ArrayList_Exercises.java/gettingDifferentColors_And_removing_2nd_element.java
@@ -1,3 +1,5 @@
+/* 1b2 Write a java program for getting different colors through ArrayList interface and remove the
+2nd element and color "Blue" from the ArrayList*/
 package java_Assignment;
 
 import java.util.ArrayList;
