c54458a5e3aef835e4b2c9e4c3bf810240c211ff
saniyaVaturi <vaturisaniya31@gmail.com>
2025-05-23 16:45:26 +0530

    thirdC2_TruncateString.java

--- 3b&3c_StrinExercise.java/thirdC2_TruncateString.java
+++ 3b&3c_StrinExercise.java/thirdC2_TruncateString.java
@@ -1,3 +1,5 @@
+/*Q7. Write a Java Program for Shortening a string to a specified length and adds an ellipsis using user
+defined function truncate()*/
 package thirdC;
 
 public class thirdC2_TruncateString {
