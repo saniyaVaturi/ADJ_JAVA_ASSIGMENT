07ce3ffc4c603a7815f3d2d722202ab10dd4e16d
saniyaVaturi <vaturisaniya31@gmail.com>
2025-05-23 16:20:30 +0530

    ArrayList_gettingDifferentColorsAndDeleting_nth.java

--- 1b_ArrayList_Exercises.java/ArrayList_gettingDifferentColorsAndDeleting_nth.java
+++ 1b_ArrayList_Exercises.java/ArrayList_gettingDifferentColorsAndDeleting_nth.java
@@ -1,3 +1,6 @@
+/* 1b1 Write a java program for getting different colors through ArrayList interface and search whether
+the color "Red" is available or not */
+
 package java_Assignment;
 import java.util.ArrayList;
 import java.util.List;
