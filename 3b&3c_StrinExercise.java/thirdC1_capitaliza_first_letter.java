ef42d6567318e7e264e5f90c8c0f9a21ba50f7ad
saniyaVaturi <vaturisaniya31@gmail.com>
2025-05-23 16:44:46 +0530

    thirdC1_capitaliza_first_letter.java

--- 3b&3c_StrinExercise.java/thirdC1_capitaliza_first_letter.java
+++ 3b&3c_StrinExercise.java/thirdC1_capitaliza_first_letter.java
@@ -1,3 +1,5 @@
+/*Q6. Write a Java Program for Capitalizing the first letter of each word. using user defined function
+capitalizeWords()*/
 package thirdC;
 
 public class thirdC1_capitaliza_first_letter {
